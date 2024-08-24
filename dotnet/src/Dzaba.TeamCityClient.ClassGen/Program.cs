7e4fd0a3ae95a77d68b7e0da06a9f0e7ceb4c588
dzaba1 <blazej.zabialowicz@gmail.com>
2024-08-24 10:17:37 +0200

    Empty ClassGen project

---
+++ dotnet/src/Dzaba.TeamCityClient.ClassGen/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Dzaba.TeamCityClient.ClassGen;
+
+internal static class Program
+{
+    public static async Task<int> Main(string[] args)
+    {
+		try
+		{
+			return 0;
+		}
+		catch (Exception ex)
+		{
+			Console.WriteLine(ex);
+			return 2;
+		}
+    }
+}
