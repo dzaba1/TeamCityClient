4b9dc83b08df82b1e90c2fe1be764a0db7835b50
dzaba1 <blazej.zabialowicz@gmail.com>
2024-08-24 15:19:31 +0200

    More tests

---
+++ dotnet/src/Dzaba.TeamCityClient.Tests/ResolveTests.cs
@@ -0,0 +1,42 @@
+﻿using FluentAssertions;
+using Microsoft.Extensions.DependencyInjection;
+using NUnit.Framework;
+using Serilog;
+
+namespace Dzaba.TeamCityClient.Tests;
+
+[TestFixture]
+public class ResolveTests
+{
+    private ServiceProvider CreateContainer()
+    {
+        var services = new ServiceCollection();
+
+        var logger = new LoggerConfiguration()
+            .MinimumLevel.Information()
+            .WriteTo.Console()
+            .CreateLogger();
+
+        services.AddLogging(l => l.AddSerilog(logger, true));
+
+        services.RegisterDzabaTeamCityClient();
+        return services.BuildServiceProvider();
+    }
+
+    [TestCase(typeof(ITeamCityClientFactory))]
+    public void GetRequiredService_WhenTypeProvided_ThenServiceIsResolved(Type serviceType)
+    {
+        using var container = CreateContainer();
+        var service = container.GetRequiredService(serviceType);
+        service.Should().NotBeNull();
+    }
+
+    [Test]
+    public void TeamCityClientFactory_WhenFactoryResolved_ThenClientCanBeCreated()
+    {
+        using var container = CreateContainer();
+        var factory = container.GetRequiredService<ITeamCityClientFactory>();
+        var client = factory.CreateClient(new TeamCityClientOptions());
+        client.Should().NotBeNull();
+    }
+}
